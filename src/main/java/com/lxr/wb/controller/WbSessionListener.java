877c39d9892a3760eb57506e35ddf0f59e4e3a49
1ssqq1lxr <1074455781@qq.com>
2017-12-27 10:53:40 +0800

    commit

--- src/main/java/com/lxr/wb/controller/WbSessionListener.java
+++ src/main/java/com/lxr/wb/controller/WbSessionListener.java
@@ -2,6 +2,7 @@ package com.lxr.wb.controller;
 
 import com.lxr.wb.domain.Advistor;
 import com.lxr.wb.service.AdvistorService;
+import org.apache.commons.lang3.time.DateFormatUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -10,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
+import java.util.Date;
 
 /**
  * 流量监听
@@ -21,6 +23,8 @@ import javax.servlet.http.HttpSessionListener;
 @WebListener
 public class WbSessionListener implements HttpSessionListener {
 
+    private final static String FORMAT_DATE = "yyyy-MM-dd HH:mm:ss";
+
     @Autowired
     private HttpServletRequest request;
 
@@ -30,8 +34,8 @@ public class WbSessionListener implements HttpSessionListener {
     @Override
     public void sessionCreated(HttpSessionEvent httpSessionEvent) {
         Advistor advisto  = new Advistor();
-        advisto.setIp("127.0.0.1");
-        advisto.setTime("2017-12-26 23:15:15");
+        advisto.setIp(getIpAddress(request));
+        advisto.setTime(DateFormatUtils.format(new Date(),FORMAT_DATE));
         Advistor save = advistorService.save(advisto);
         HttpSession session = httpSessionEvent.getSession();
         System.out.println("session create:"+session.getId());
@@ -41,5 +45,39 @@ public class WbSessionListener implements HttpSessionListener {
     public void sessionDestroyed(HttpSessionEvent httpSessionEvent) {
         HttpSession session = httpSessionEvent.getSession();
         System.out.println("session :"+session.getId());
+    }
+    /**
+     * 获取用户真实IP地址，不使用request.getRemoteAddr();的原因是有可能用户使用了代理软件方式避免真实IP地址,
+     * 参考文章： http://developer.51cto.com/art/201111/305181.htm
+     *
+     * 可是，如果通过了多级反向代理的话，X-Forwarded-For的值并不止一个，而是一串IP值，究竟哪个才是真正的用户端的真实IP呢？
+     * 答案是取X-Forwarded-For中第一个非unknown的有效IP字符串。
+     *
+     * 如：X-Forwarded-For：192.168.1.110, 192.168.1.120, 192.168.1.130,
+     * 192.168.1.100
+     *
+     * 用户真实IP为： 192.168.1.110
+     *
+     * @param request
+     * @return
+     */
+    public static String getIpAddress(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return ip;
     }
 }
